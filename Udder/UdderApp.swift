11c6ccb4ebd90adaf6474ba5070119029d7618d9
Elisabeth Stecki <lisa@whineware.com>
2022-03-21 05:58:35 -0700

    Initial Commit

---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by Elisabeth Stecki on 3/21/22.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
